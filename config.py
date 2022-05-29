da92e70562df3c3e1d6a1d0b9ba918483114b397
SEMUL <semul.off@gmail.com>
2022-05-30 00:30:11 +0300

    beta version 1.0: added authorization; employment (without captcha)

--- config.py
+++ config.py
@@ -1,12 +1,25 @@
-# URL
-url = "https://www.heroeswm.ru/home.php"
-
 # Path to webdrivee
-webdriver_path = "/home/semul/Desktop/HeroesWM_employment/chromedriver/driver"
-
-# key - Nickname; values - ID
-account_ID = {"Nickname": "ID"}
+webdriver_path = "./chromedriver/driver"
 
 # key - ID; value - user-agent_pattern
+# [Insert your values]
 user_agents = {"ID": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36"}
 
+# URLs
+urls = {"main": "https://www.heroeswm.ru", "login": "https://www.heroeswm.ru/login.php", "map": "https://www.heroeswm.ru/map.php"}
+
+# key - Nickname; values - ID
+# [Insert your values]
+account_ID = {"Nickname": "ID"}
+
+# login and password
+# [Insert your values]
+data = {"ID": {"login": "your_nickname", "password": "your_password"}}
+
+# the current location of the character on the map (what area?)
+# [Insert your values]
+# Example: For district "Rogues' Wood" value - "cx=51&cy=49" 
+current_district = "cx=51&cy=49"
+
+# structure of enterprises to work
+structures = ["sh", "fc", "mn"]
--- main.py
+++ main.py
@@ -4,21 +4,74 @@ from config import *
 
 
 
-# options
-options = webdriver.ChromeOptions()
-options.add_argument(f"user-agent={user_agents['ID']}")
+def main():
+    try:
+        authorization()
+        sleep(5)
+        employ()
+    except Exception as _ex:
+        print(f'[Error!] {_ex}')
+    finally:
+        driver.close()
+        driver.quit()
 
-driver = webdriver.Chrome(
-    executable_path=webdriver_path, 
-    options=options
-    )
+
+# account authorization
+def authorization():
+    driver.get(url=urls["main"])
+    
+    try:
+        login = driver.find_element_by_class_name("inp_login")
+        login.clear()
+        login.send_keys(data["ID"]["login"])
+
+        password = driver.find_element_by_class_name("inp_pass")
+        password.clear()
+        password.send_keys(data["ID"]["password"])
+
+        log_in = driver.find_element_by_class_name("entergame")
+        log_in.click()
+    except Exception as _ex:
+        print("[Authorization error]")
+
+# character employment
+def employ():
+    driver.get(url=urls["map"])
+ 
+    for structure in structures:
+        url = urls["map"] + "?" + current_district + "&st=" + structure
+        driver.get(url)
+
+        try:
+            concern_link = driver.find_element_by_link_text("»»»")
+            concern_link.click()
+
+            captcha_solution()
+
+            if "object-info" in driver.current_url:
+                break
+        except Exception as _ex:
+            print(f"[Mistake in employment] {_ex}")
+ 
+    print("[Done!]")        
 
 
-try:
-    driver.get(url=url)
-    sleep(5)
-except Exception as _ex:
-    print(f'[Error!] {_ex}')
-finally:
-    driver.close()
-    driver.quit()
+def captcha_solution():
+    try:
+        getjob = driver.find_element_by_xpath('//*[@id="wbtn"]')
+        getjob.click()
+    except Exception as _ex:
+        print("[Can't find button]")
+
+
+if __name__ == "__main__":
+    # options
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"user-agent={user_agents['ID']}")
+
+    driver = webdriver.Chrome(
+        executable_path=webdriver_path, 
+        options=options
+        )
+
+    main()
