bbfcee398326f07eab79153f5132d6009fe47ac4
SEMUL <semul.off@gmail.com>
2022-05-31 02:08:42 +0300

    improved the logic of functions, and also added automatic determination of the unique coordinates of the character on the map

--- config.py
+++ config.py
@@ -14,12 +14,37 @@ account_ID = {"Nickname": "ID"}
 
 # login and password
 # [Insert your values]
-data = {"ID": {"login": "your_nickname", "password": "your_password"}}
+data = {"ID": {"login": "<your_nickname>", "password": "<your_password>"}}
 
-# the current location of the character on the map (what area?)
-# [Insert your values]
-# Example: For district "Rogues' Wood" value - "cx=51&cy=49" 
-current_district = "cx=51&cy=49"
+# The game has a unique coordinate for each area.
+districts = {
+    "Ungovernable Steppe": "cx=48&cy=48", 
+    "Eagle Nest": "cx=49&cy=48", 
+    "Peaceful Camp": "cx=50&cy=48",
+    "Crystal Garden": "cx=51&cy=48",
+    "Fairy Trees": "cx=52&cy=48",
+    "Sunny City": "cx=48&cy=49",
+    "Shining Spring": "cx=49&cy=49",
+    "Tiger Lake": "cx=50&cy=49",
+    "Rogues' Wood": "cx=51&cy=49",
+    "Bear Mountain": "cx=52&cy=49",
+    "Mithril Coast": "cx=53&cy=49",
+    "Sublime Arbor": "cx=48&cy=50",
+    "Green Wood": "cx=49&cy=50",
+    "Empire Capital": "cx=50&cy=50",
+    "East River": "cx=51&cy=50",
+    "Magma Mines": "cx=52&cy=50",
+    "Harbour City": "cx=53&cy=50",
+    "Lizard Lowland": "cx=49&cy=51",
+    "Wolf Dale": "cx=50&cy=51",
+    "Dragons' Caves": "cx=51&cy=51",
+    "The Wilderness": "cx=49&cy=52",
+    "Portal Ruins": "cx=50&cy=52",
+    "Great Wall": "cx=51&cy=52",
+    "Titans' Valley": "cx=51&cy=53",
+    "Fishing Village": "cx=52&cy=53",
+    "Kingdom Castle": "cx=52&cy=54"
+    }
 
 # structure of enterprises to work
 structures = ["sh", "fc", "mn"]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -5,23 +6,17 @@ from config import *
 
 
 
-def main() -> None:
-    try:
-        autorization()
-        sleep(10)
-        employ()
-    except Exception as _ex:
-        print(f'[Error!] {_ex}')
-    finally:
-        driver.close()
-        driver.quit()
+# Account autorization
+def autorization() -> None:
+    print(">> Autorization... ")
+    autorization_process()
+    sleep(1)
+    
+    if driver.current_url == urls["login"]:
+        autorization_process(flag=True)
 
 
-def detect_captcha() -> str:
-    pass
-
-
-# Autorization function: if you need to enter captcha or without. flag = False -> without captcha, else -> with
+# Autorization function: if you need to enter captcha or without. flag = True -> captcha present, else -> no captcha
 def autorization_process(flag=False) -> None: 
     try:
         if flag == False:
@@ -29,12 +24,12 @@ def autorization_process(flag=False) -> None:
             login = driver.find_element(By.CLASS_NAME, "inp_login")
             login.clear()
             login.send_keys(data["ID"]["login"])
-            sleep(2)
+            sleep(1)
 
             password = driver.find_element(By.CLASS_NAME, "inp_pass")
             password.clear()
             password.send_keys(data["ID"]["password"])
-            sleep(2)
+            sleep(1)
 
             log_in = driver.find_element(By.CLASS_NAME, "entergame")
             log_in.click()
@@ -42,15 +37,16 @@ def autorization_process(flag=False) -> None:
             login = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[1]/td[2]/input")
             login.clear()
             login.send_keys(data["ID"]["login"])
-            sleep(2)
+            sleep(1)
 
             password = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[2]/td[2]/input")
             password.clear()
             password.send_keys(data["ID"]["password"])
-            sleep(2)
+            sleep(1)
 
+            # EDIT
             # captcha = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[4]/td/table/tbody/tr/td[1]/img")
-            # code = detect_captcha(captcha)
+            # code = captcha_solution(src_link=, flag=True)
             # sleep(5)
 
             # input_code_form = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[4]/td/table/tbody/tr/td[2]/input")
@@ -60,53 +56,63 @@ def autorization_process(flag=False) -> None:
             log_in = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[5]/td/input[1]")
             log_in.click()
             
-        print(">> [Done]")
+        print(">> [Successful authorization!]")
     except Exception as _ex:
         print(">> [Autorization error]: {_ex}")
 
 
-# Account autorization
-def autorization() -> None:
-    print(">> Autorization... ")
-    autorization_process()
-    sleep(2)
-    
-    if driver.current_url == urls["login"]:
-        autorization_process(flag=True)
+# Captcha solution: flag = True -> captcha present, else -> there is only a button
+def captcha_solution(src_link=None, flag=True) -> Union[str, None]:
+    if flag == True:
+        pass
+    else:
+        try:
+            getjob = driver.find_element(By.XPATH, '//*[@id="wbtn"]')
+            getjob.click()
+        except:
+            print("[Are you employed]")
 
 
 # character employment
 def employ() -> None:
-    print(">> Employment... ") 
-    driver.get(url=urls["map"])
- 
+    print(">> Employment... ")
+
+    driver.get(url=urls["map"]) 
+    current_district = determination_current_district()
+
     for structure in structures:
         url = urls["map"] + "?" + current_district + "&st=" + structure
         driver.get(url)
-
         try:
             concern_link = driver.find_element(By.LINK_TEXT, "»»»")
             concern_link.click()
-
-            captcha_solution()
-            
-            print("[Done!]")
+            # check: is there a captcha?
+            # EDIT
+            captcha_solution(flag=False) 
+            print(">> [Successful employment!]")
 
             if "object-info" in driver.current_url:
                 break
         except Exception as _ex:
-            print(f"[Mistake in employment] {_ex}")
- 
-           
+            print("[No free work]")
 
 
-# temporary function
-def captcha_solution() -> None:
+# Defines a unique coordinate for a character based on its location
+def determination_current_district() -> str:
+    district_name = driver.find_element(By.XPATH, '//*[@id="set_mobile_max_width"]/div[1]/b').text 
+    return districts[district_name]
+
+
+def main() -> None:
     try:
-        getjob = driver.find_element(By.XPATH, '//*[@id="wbtn"]')
-        getjob.click()
+        autorization()
+        sleep(5)
+        employ()
     except Exception as _ex:
-        print("[You are employed]")
+        print(f'[Error!] {_ex}')
+    finally:
+        driver.close()
+        driver.quit()
 
 
 if __name__ == "__main__":
