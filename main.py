70feb3e087dd20ac54025f641989b6c01540d898
SEMUL <semul.off@gmail.com>
2022-05-30 18:24:09 +0300

    Review code and added terms of reference

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 from config import *
 
@@ -7,7 +8,7 @@ from config import *
 def main() -> None:
     try:
         autorization()
-        sleep(5)
+        sleep(10)
         employ()
     except Exception as _ex:
         print(f'[Error!] {_ex}')
@@ -25,50 +26,51 @@ def autorization_process(flag=False) -> None:
     try:
         if flag == False:
             driver.get(url=urls["main"])
-            login = driver.find_element_by_class_name("inp_login")
+            login = driver.find_element(By.CLASS_NAME, "inp_login")
             login.clear()
             login.send_keys(data["ID"]["login"])
-            sleep(5)
+            sleep(2)
 
-            password = driver.find_element_by_class_name("inp_pass")
+            password = driver.find_element(By.CLASS_NAME, "inp_pass")
             password.clear()
             password.send_keys(data["ID"]["password"])
-            sleep(5)
+            sleep(2)
 
-            log_in = driver.find_element_by_class_name("entergame")
+            log_in = driver.find_element(By.CLASS_NAME, "entergame")
             log_in.click()
         else:
-            login = driver.find_element_by_xpath("/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[1]/td[2]/input")
+            login = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[1]/td[2]/input")
             login.clear()
             login.send_keys(data["ID"]["login"])
-            sleep(5)
+            sleep(2)
 
-            password = driver.find_element_by_xpath("/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[2]/td[2]/input")
+            password = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[2]/td[2]/input")
             password.clear()
             password.send_keys(data["ID"]["password"])
-            sleep(20)
+            sleep(2)
 
-            # captcha = driver.find_element_by_xpath("/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[4]/td/table/tbody/tr/td[1]/img")
+            # captcha = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[4]/td/table/tbody/tr/td[1]/img")
             # code = detect_captcha(captcha)
             # sleep(5)
 
-            # input_code_form = driver.find_element_by_xpath("/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[4]/td/table/tbody/tr/td[2]/input")
+            # input_code_form = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[4]/td/table/tbody/tr/td[2]/input")
             # input_code_form.send_keys(code)
             # sleep(5)
 
-            log_in = driver.find_element_by_xpath("/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[5]/td/input[1]")
+            log_in = driver.find_element(By.XPATH, "/html/body/center/table/tbody/tr/td/table/tbody/tr/td/form/table/tbody/tr[5]/td/input[1]")
             log_in.click()
             
-            print("[Done]")
+        print(">> [Done]")
     except Exception as _ex:
-        print("[Autorization error]")
+        print(">> [Autorization error]: {_ex}")
 
 
 # Account autorization
 def autorization() -> None:
     print(">> Autorization... ")
     autorization_process()
-    sleep(5)
+    sleep(2)
+    
     if driver.current_url == urls["login"]:
         autorization_process(flag=True)
 
@@ -83,7 +85,7 @@ def employ() -> None:
         driver.get(url)
 
         try:
-            concern_link = driver.find_element_by_link_text("»»»")
+            concern_link = driver.find_element(By.LINK_TEXT, "»»»")
             concern_link.click()
 
             captcha_solution()
@@ -101,7 +103,7 @@ def employ() -> None:
 # temporary function
 def captcha_solution() -> None:
     try:
-        getjob = driver.find_element_by_xpath('//*[@id="wbtn"]')
+        getjob = driver.find_element(By.XPATH, '//*[@id="wbtn"]')
         getjob.click()
     except Exception as _ex:
         print("[You are employed]")
